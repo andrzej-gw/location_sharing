23c1f2432da1aac33d3ca26de4a1453d326acadc
Andrzej Gwiazda <jedrekgwiazda@gmail.com>
2024-10-03 21:03:57 +0200

    Personalized data dictionary.

--- prog.py
+++ prog.py
@@ -7,19 +7,15 @@ import pyais
 import extract_cookies
 import latlon
 
-data = {
-    'mmsi': '30000000',
-    'type': 19,
-}
-
 
 cookies_file = 'cookies.txt'
 extract_cookies.extract_cookies(cookies_file)
 
 google_email = 'land.rover.discoveryii2002@gmail.com'
-#  google_email = 'emilia.partyka@gmail.com'
 
 people = {}
+
+data={}
 
 while True:
     service = Service(cookies_file=cookies_file, authenticating_account=google_email)
@@ -27,11 +23,15 @@ while True:
     for person in service.get_all_people():
         #  print(person.nickname)
         #  print(person.latitude)
-        
-        data['lat']=person.latitude
-        data['lon']=person.longitude
-        data['shipname']=unidecode(person.nickname)
-        data['mmsi']=str(i)
+        if not person.nickname in data:
+            data[person.nickname]={
+                'mmsi': '30000000',
+                'type': 19,
+            }
+        data[person.nickname]['lat']=person.latitude
+        data[person.nickname]['lon']=person.longitude
+        data[person.nickname]['shipname']=unidecode(person.nickname)
+        data[person.nickname]['mmsi']=str(i)
         #  print(person.datetime)
         #  print(person.timestamp)
         if person.nickname in people:
@@ -49,13 +49,13 @@ while True:
                 heading=round(A.heading_initial(B), 2)%360
                 speed=round(distance_nm/time_in_hours, 2)
                 #  print("speed", speed)
-                data['speed']=str(speed)
+                data[person.nickname]['speed']=str(speed)
                 #  print("heading", heading)
-                data['heading']=int(heading)
-                data['course']=str(heading)
+                data[person.nickname]['heading']=int(heading)
+                data[person.nickname]['course']=str(heading)
         people[person.nickname]=(person.latitude, person.longitude, person.timestamp)
         i+=1
-        encoded = pyais.encode_dict(data, radio_channel="B", talker_id="AIVDM")
+        encoded = pyais.encode_dict(data[person.nickname], radio_channel="B", talker_id="AIVDM")
         print(encoded[0])
         sys.stdout.flush() 
         #nc -u 0.0.0.0 1234
